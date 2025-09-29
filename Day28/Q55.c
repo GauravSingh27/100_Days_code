e14db9394b5df3efd7807a500db07ab242c5c21a
GauravSingh27 <win4gaurav@gmail.com>
2025-09-29 20:47:47 +0530

    Add files via upload

---
+++ Day28/Q55.c
@@ -0,0 +1,28 @@
+/*Q55 (Nested Loops without Arrays/Strings)
+Write a program to print all the prime numbers from 1 to n.*/
+#include <stdio.h>
+int main() 
+{
+    int n, i, j, flag;
+    
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Prime numbers from 1 to %d are:\n", n);
+    for(i = 2; i <= n; i++) {
+        flag = 1; 
+        for(j = 2; j <= i/2; j++) {
+            if(i % j == 0) {
+                flag = 0; 
+                break;
+            }
+        }
+        if(flag == 1) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
+
