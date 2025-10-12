0f24777db2b56d7d81e17de0d8ac676bc10f077e
GauravSingh27 <win4gaurav@gmail.com>
2025-10-12 08:42:48 +0530

    Add files via upload

---
+++ Day42/Q83.c
@@ -0,0 +1,30 @@
+/*Q83 (Strings)
+Count vowels and consonants in a string.*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int vowels = 0, consonants = 0, i = 0;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0') {
+        char ch = str[i];
+
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            char lower = (ch >= 'A' && ch <= 'Z') ? ch + 32 : ch;
+            if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u') {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+        i++;
+    }
+
+    printf("Vowels: %d\n", vowels);
+    printf("Consonants: %d\n", consonants);
+
+    return 0;
+}
+
